86aa17387b618178c7f9a1948b3782495c8ac2d7
Abhay123s <102819184+Abhay123s@users.noreply.github.com>
2023-12-28 19:33:31 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -84,19 +84,18 @@ public class Main {
     	public static void main(String[] args) {
         	List<Question> questions = new ArrayList<>();
 
-        	questions.add(new Question("What is the capital of India?", new String[]{"Mumbai", "Delhi", "Kolkata", "Nagpur"}, 1));
-        	questions.add(new Question("What is the largest planet in our solar system?", new String[]{"Earth", "Jupiter", "Mars", "Venus"}, 1));
-        	questions.add(new Question("What is the chemical symbol for water?", new String[]{"H20", "CO2", "O2", "N2"}, 0));
+	        	questions.add(new Question("What is the capital of India?", new String[]{"Mumbai", "Delhi", "Kolkata", "Nagpur"}, 1));
+	        	questions.add(new Question("What is the largest planet in our solar system?", new String[]{"Earth", "Jupiter", "Mars", "Venus"}, 1));
+	        	questions.add(new Question("What is the chemical symbol for water?", new String[]{"H20", "CO2", "O2", "N2"}, 0));
 			questions.add(new Question("Wh0 is first PM of India?", new String[]{"Mahatma Gandhi", "Pandit Nehru2", "Sardar Patel", "S.C. Bose"}, 1));
-        	questions.add(new Question("What is the largest ocean on Earth?", new String[]{"Atlantic Ocean", "Indian Ocean", "Southern Ocean", "Pacific Ocean"}, 3));
+	        	questions.add(new Question("What is the largest ocean on Earth?", new String[]{"Atlantic Ocean", "Indian Ocean", "Southern Ocean", "Pacific Ocean"}, 3));
 			questions.add(new Question("Who is known as the 'Father of Computer Science'?", new String[]{"Alan Turing", "Bill Gates", "Steve Jobs", "Tim Berners-Lee"}, 0));
 			questions.add(new Question("What is the capital of Australia?", new String[]{"Canberra", "Sydney", "Melbourne", "Brisbane"}, 0));
 			questions.add(new Question("Which famous scientist developed the theory of general relativity?", new String[]{"Isaac Newton", "Albert Einstein", "Stephen Hawking", "Galileo Galilei"}, 1));
 			questions.add(new Question("What is the largest mammal in the world?", new String[]{"Elephant", "Blue Whale", "Giraffe", "Polar Bear"}, 1));
-				questions.add(new Question("Who is Known as Ironman of India?", new String[]{"Sarojani Naidu1
-				", "Sardar Patel", "Motilal Nehru", "Mahatma Gandhi"}, 1));
+			questions.add(new Question("Who is Known as Ironman of India?", new String[]{"Sarojani Naidu1", "Sardar Patel", "Motilal Nehru", "Mahatma Gandhi"}, 1));
 			
-			Quiz quiz = new Quiz(questions);
+		Quiz quiz = new Quiz(questions);
         	quiz.startQuiz();
     	}
 }
